f3a9fd20396b7c1793fe886642f2949286d39aed
dat1806 <dtd1806@gmail.com>
2026-08-17 20:28:48 +0700

    refactor: remove debug log from App component

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,6 @@ export const App: React.FC = () => {
 
   // Theme Class Toggle
   useEffect(() => {
-    console.log('----')
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
     } else {
